7bb9a9724d93bfd6555309cf158fc34d016ee299
quantumgitexpert <75207681+quantumgitexpert@users.noreply.github.com>
2025-08-16 21:00:36 +0200

    changed echo to ui_print

--- action.sh
+++ action.sh
@@ -2,13 +2,13 @@
 
 MODDIR="$(dirname "$0")"  # /data/adb/modules/<module-id>
 
-echo "- Starting action for $(basename "$MODDIR")"
+ui_print "- Starting copy for $(basename "$MODDIR")"
 
 # create folder if necessary to store the data
 mkdir -p /storage/emulated/0/ViewOnce
 # copy data to said folder in the internal storage
 cp -r /data/data/com.whatsapp/files/ViewOnce/* /storage/emulated/0/ViewOnce/
 
-echo "- Action completed!"
+ui_print "- Copy completed!"
 
 exit 0
