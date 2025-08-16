4c84711b40ca1da69616431a5f95aa0c4c64d8f1
quantumgitexpert <75207681+quantumgitexpert@users.noreply.github.com>
2025-08-16 21:17:52 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,13 +2,13 @@
 
 MODDIR="$(dirname "$0")"  # /data/adb/modules/<module-id>
 
-ui_print "- Starting copy for $(basename "$MODDIR")"
+echo -e "\nStarting copy for $(basename "$MODDIR")"
 
 # create folder if necessary to store the data
 mkdir -p /storage/emulated/0/ViewOnce
 # copy data to said folder in the internal storage
 cp -r /data/data/com.whatsapp/files/ViewOnce/* /storage/emulated/0/ViewOnce/
 
-ui_print "- Copy completed!"
+echo -e "\nCopy completed!"
 
 exit 0
