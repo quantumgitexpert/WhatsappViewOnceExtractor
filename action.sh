1f79c7c8af136f6282e9f3320f0a66dcb6e730f3
quantumgitexpert <75207681+quantumgitexpert@users.noreply.github.com>
2025-08-16 20:48:37 +0200

    Create action.sh

---
+++ action.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+MODDIR="$(dirname "$0")"  # /data/adb/modules/<module-id>
+
+echo "- Starting action for $(basename "$MODDIR")"
+
+# create folder if necessary to store the data
+mkdir -p /storage/emulated/0/ViewOnce
+# copy data to said folder in the internal storage
+cp -r /data/data/com.whatsapp/files/ViewOnce/* /storage/emulated/0/ViewOnce/
+
+echo "- Action completed!"
+exit 0
